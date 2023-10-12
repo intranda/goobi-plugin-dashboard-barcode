1ad47a5068d5f1284f3bd2531a722566c7ad0e7b
zehong <zehong.hu@intranda.com>
2023-10-12 20:07:48 +0200

    sketch of the barcode form; stuck with a Conversion Error here...

--- plugin/src/main/java/de/intranda/goobi/plugins/BarcodeDashboardPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/BarcodeDashboardPlugin.java
@@ -1,8 +1,11 @@
 package de.intranda.goobi.plugins;
 
 import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import org.apache.commons.configuration.XMLConfiguration;
 import org.goobi.production.enums.PluginType;
@@ -12,7 +15,9 @@ import de.intranda.digiverso.model.helper.DashboardHelperBarcode;
 import de.intranda.digiverso.model.helper.DashboardHelperTasks;
 import de.sub.goobi.config.ConfigPlugins;
 import de.sub.goobi.helper.FacesContextHelper;
+import de.sub.goobi.helper.Helper;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 
@@ -38,6 +43,13 @@ public class BarcodeDashboardPlugin implements IDashboardPlugin {
 
     private transient DashboardHelperTasks tasksHelper;
     private transient DashboardHelperBarcode barcodeHelper;
+
+    @Getter
+    @Setter
+    private String currentStep;
+
+    @Setter
+    private List<String> choices;
 
     /**
      * Constructor
@@ -99,6 +111,13 @@ public class BarcodeDashboardPlugin implements IDashboardPlugin {
 
     public void execute() {
         log.info("hello world");
+    }
+
+    public List<String> getChoices() {
+        String choice1 = "plugin_dashboard_barcode_takeOrFinishTask";
+        String choice2 = "plugin_dashboard_barcode_takeAndFinishTask";
+        String choice3 = "plugin_dashboard_barcode_changeLocationOnly";
+        return Arrays.asList(choice1, choice2, choice3).stream().map(Helper::getTranslation).collect(Collectors.toList());
     }
 
     public String getOutput() {
